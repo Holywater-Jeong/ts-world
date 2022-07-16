870820f074187e997e5d3f29d7ab23b15e59edf1
Seongsu Stephen Jeong <holywaterj0410@gmail.com>
2022-07-16 17:12:08 +0900

    fix: `Button` Comp. & used code
    add: workspace alias script

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>정성수의 모노레포</h1>
+      <Button onClick={() => alert('버튼')}>버튼</Button>
     </div>
   );
 }
