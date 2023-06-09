51eb113bb50d4a0be22f565cd2fcb18c6edc8a62
Seongsu Jeong <holywaterj0410@gmail.com>
2023-06-09 18:28:40 +0900

    add: Prisma Adapter

--- apps/share-my-plan/src/lib/auth.ts
+++ apps/share-my-plan/src/lib/auth.ts
@@ -1,5 +1,9 @@
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -9,4 +13,5 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
 };
