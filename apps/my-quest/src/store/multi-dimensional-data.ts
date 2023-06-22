049fadbd38618dfe69a396209412f990ff150c05
Seongsu Jeong <holywaterj0410@gmail.com>
2023-06-22 13:27:06 +0900

    fix Type

--- apps/my-quest/src/store/multi-dimensional-data.ts
+++ apps/my-quest/src/store/multi-dimensional-data.ts
@@ -10,12 +10,12 @@ import {
   ItemOrderType,
 } from '~/types/multi-dimensional-data';
 
-export const tabsAtom = atom<TabMap | null>(null);
+export const tabsAtom = atom<TabMap>(new Map());
 export const tabsOrdersAtom = atom<TabOrderType>([]);
 export const selectedTabPkAtom = atom<ComponentsPk>(0);
 
-export const templatesAtom = atom<TemplateMap | null>(null);
-export const templatesOrdersAtom = atom<TemplateOrderType | null>(null);
+export const templatesAtom = atom<TemplateMap>(new Map());
+export const templatesOrdersAtom = atom<TemplateOrderType>(new Map());
 
-export const itemsAtom = atom<ItemMap | null>(null);
-export const itemsOrdersAtom = atom<ItemOrderType | null>(null);
+export const itemsAtom = atom<ItemMap>(new Map());
+export const itemsOrdersAtom = atom<ItemOrderType>(new Map());
